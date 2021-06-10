c3bd0db8b03293a42b8b8bbdef7d89df86835d57
Alex <mil885336@gmail.com>
2021-06-10 07:15:28 -0400

    Getting basic files together

---
+++ rockps.js
@@ -0,0 +1,28 @@
+function welcome() {
+    console.log("Welcome");
+}
+
+
+//Step 2
+//Select what computer will play - rock, paper, or scissors
+function computerPlay() {
+    //screate array with the values
+    //pick a random number from 0-2
+    //set computer selection to arr[x]
+}
+
+//Get user input for their selection
+function userPlay() {
+    //ask for selection
+    //put in all lowercase
+    //if it doesnt equal rock paper or scissors
+        //ask for another selection
+        //else set userselection variable
+}
+
+
+//Step 4
+//Play a round of RPS
+function playRound() {
+
+}
